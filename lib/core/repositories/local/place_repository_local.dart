e9290cb85502c0b6a9600106d1f20a07c7a22dd7
Calvin Ren <calvin.qut@gmail.com>
2024-03-27 11:42:08 +1000

    create local place repository

---
+++ lib/core/repositories/local/place_repository_local.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_code_test_dropdown_buttons/core/repositories/place_repository.dart';
+import 'package:flutter_code_test_dropdown_buttons/models/data_models/country/country.dart';
+import 'package:flutter_code_test_dropdown_buttons/models/data_models/state/state.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class PlaceRepositoryLocal extends PlaceRepository {
+  PlaceRepositoryLocal(this.ref);
+  final Ref ref;
+
+  @override
+  Future<List<Country>> getCountries() {
+    // TODO: implement getCountries
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<State>> getStates() {
+    // TODO: implement getStates
+    throw UnimplementedError();
+  }
+}
